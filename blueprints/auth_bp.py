dc8bf9a4edf0ebdc75f08164542987d1ec9af207
arharuty <13937@coderacademy.edu.au>
2023-06-27 23:03:48 +1000

    register route, admin required auth

--- blueprints/auth_bp.py
+++ blueprints/auth_bp.py
@@ -15,49 +15,50 @@ def all_users():
     return UserSchema(many=True, exclude=['password']).dump(users)
 
 
-# @auth_bp.route('/register', methods=['POST'])
-# def register():
-#     try:
-#         # Parse, sanitize and validate the incoming JSON data
-#         # via the schema
-#         user_info = UserSchema().load(request.json)
-#         # Create a new User model instance with the schema data
-#         user = User(
-#             email=user_info['email'],
-#             password=bcrypt.generate_password_hash(user_info['password']).decode('utf-8'),
-#             name=user_info['name']
-#         )
+@auth_bp.route('/register', methods=['POST'])
+def register():
+    try:
+        # Parse, sanitize and validate the incoming JSON data
+        # via the schema
+        user_info = UserSchema().load(request.json)
+        # Create a new User model instance with the schema data
+        user = User(
+            email=user_info['email'],
+            password=bcrypt.generate_password_hash(user_info['password']).decode('utf-8'),
+            first_name=user_info['first_name'],
+            last_name=user_info['last_name']
+        )
 
-#         # Add and commit the new user
-#         db.session.add(user)
-#         db.session.commit()
+        # Add and commit the new user
+        db.session.add(user)
+        db.session.commit()
 
-#         # Return the new user, excluding the password
-#         return UserSchema(exclude=['password']).dump(user), 201
-#     except IntegrityError:
-#         return {'error': 'Email address already in use'}, 409
-
-
-# @auth_bp.route('/login', methods=['POST'])
-# def login():
-#     try:
-#         stmt = db.select(User).filter_by(email=request.json['email'])
-#         user = db.session.scalar(stmt)
-#         if user and bcrypt.check_password_hash(user.password, request.json['password']):
-#             token = create_access_token(identity=user.id, expires_delta=timedelta(days=1))
-#             return {'token': token, 'user': UserSchema(exclude=['password', 'cards', 'comments']).dump(user)}
-#         else:
-#             return {'error': 'Invalid email address or password'}, 401
-#     except KeyError:
-#         return {'error': 'Email and password are required'}, 400
+        # Return the new user, excluding the password
+        return UserSchema(exclude=['password']).dump(user), 201
+    except IntegrityError:
+        return {'error': 'Email address already in use'}, 409
 
 
-# def admin_required():
-#   user_id = get_jwt_identity()
-#   stmt = db.select(User).filter_by(id=user_id)
-#   user = db.session.scalar(stmt)
-#   if not (user and user.is_admin):
-#     abort(401, description="You must be an admin.")
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    try:
+        stmt = db.select(User).filter_by(email=request.json['email'])
+        user = db.session.scalar(stmt)
+        if user and bcrypt.check_password_hash(user.password, request.json['password']):
+            token = create_access_token(identity=user.id, expires_delta=timedelta(days=1))
+            return {'token': token, 'user': UserSchema(exclude=['password']).dump(user)}
+        else:
+            return {'error': 'Invalid email address or password'}, 401
+    except KeyError:
+        return {'error': 'Email and password are required'}, 400
+
+
+def admin_required():
+  user_id = get_jwt_identity()
+  stmt = db.select(User).filter_by(id=user_id)
+  user = db.session.scalar(stmt)
+  if not (user and user.is_admin):
+    abort(401, description="You must be an admin.")
 
 # def admin_or_owner_required(owner_id):
 #   user_id = get_jwt_identity()
--- main.py
+++ main.py
@@ -5,16 +5,18 @@ from flask_sqlalchemy import SQLAlchemy
 from init import db, ma, bcrypt, jwt
 from blueprints import registerable_bp
 
-# db = SQLAlchemy()
-
 def setup():
     app = Flask(__name__)
 
-    # app.config['JWT_SECRET_KEY'] = environ.get('JWT_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URI')
+    app.config['JWT_SECRET_KEY'] = "Mushu is the Best"
+    
+    # environ.get('JWT_KEY')
 
     db.init_app(app)
     ma.init_app(app)
+    jwt.init_app(app)
+    bcrypt.init_app(app)
 
     for bp in registerable_bp:
         app.register_blueprint(bp)
