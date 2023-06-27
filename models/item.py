89d68ef649ea413454cb4107ec5af6a5544620a9
arharuty <13937@coderacademy.edu.au>
2023-06-28 02:40:10 +1000

    item model added

---
+++ models/item.py
@@ -0,0 +1,15 @@
+from init import db
+
+class Item(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.String(30), nullable=False)
+    item_desc = db.Column(db.Text())
+    date_created = db.Column(db.Date())
+
+    property_id = db.Column(db.Integer(), db.ForeignKey('properties.id', ondelete='CASCADE'))
+    property = db.relationship('Property', back_populates='items')
+
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship('User', back_populates='items')
