4e9bf62f4f4242fe6d18f6f8c2f00bd5160708c3
arharuty <13937@coderacademy.edu.au>
2023-06-27 22:19:11 +1000

    separate registerable bps to own file

---
+++ blueprints/__init__.py
@@ -0,0 +1,7 @@
+from blueprints.cli_bp import cli_bp
+from blueprints.auth_bp import auth_bp
+
+registerable_bp = [
+    cli_bp,
+    auth_bp
+]
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from blueprints.cli_bp import cli_bp
 from os import environ
 from flask_sqlalchemy import SQLAlchemy
 from init import db, ma, bcrypt, jwt
+from blueprints import registerable_bp
 
 # db = SQLAlchemy()
 
@@ -13,8 +14,10 @@ def setup():
     app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URI')
 
     db.init_app(app)
+    ma.init_app(app)
 
-    app.register_blueprint(cli_bp)
+    for bp in registerable_bp:
+        app.register_blueprint(bp)
 
     return app
         
