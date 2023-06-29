74fbe48f33647434db980d55990ee7c3d34dcb44
arharuty <13937@coderacademy.edu.au>
2023-06-29 16:05:45 +1000

    fix item schema to show comments nested

--- blueprints/prop_bp.py
+++ blueprints/prop_bp.py
@@ -5,6 +5,8 @@ from models.propertyuser import PropertyUser
 from schemas.role_schema import RoleSchema
 from models.item import Item
 from schemas.item_schema import ItemSchema
+from models.comment import Comment
+from schemas.comment_schema import CommentSchema
 from init import db
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from blueprints.auth_bp import admin_required, access_required
--- schemas/item_schema.py
+++ schemas/item_schema.py
@@ -2,10 +2,11 @@ from init import ma
 from marshmallow import fields
 
 class ItemSchema(ma.Schema):
-    class Meta:
-        user = fields.Nested('UserSchema', only=['first_name', 'last_name'])
+    user = fields.Nested('UserSchema', only=['first_name', 'last_name'])
+    comments = fields.List(fields.Nested('CommentSchema', exclude=['user', 'item']))
 
-        fields = ('item_name', 'item_desc', 'id', 'user')
+    class Meta:
+        fields = ('item_name', 'item_desc', 'id', 'user', 'comments')
 
 item_schema = ItemSchema()
 itemss_schema = ItemSchema(many=True)
