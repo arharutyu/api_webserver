07f578cc9bf08f53c47c62323e767b7a4f8b5e70
arharuty <13937@coderacademy.edu.au>
2023-07-04 04:34:56 +1000

    validate role field in role schema

--- schemas/role_schema.py
+++ schemas/role_schema.py
@@ -1,6 +1,7 @@
 from init import ma
 from marshmallow import fields, validates_schema
 from marshmallow.exceptions import ValidationError
+from marshmallow.validate import OneOf
 
 VALID_ROLES = ['Unassigned', 'Property Manager', 'Tenant']
 
@@ -8,16 +9,7 @@ class RoleSchema(ma.Schema):
   user = fields.Nested('UserSchema', only=['first_name', 'last_name', 'id'])
   property = fields.Nested('PropertySchema')
   role = fields.String(load_default=VALID_ROLES[0])
-  
 
-  # @validates_schema()
-  # def validate_status(self, data, **kwargs):
-  #   role = [x for x in VALID_ROLES if x.upper() == data['role'].upper()]
-  #   if len(role) == 0:
-  #      raise ValidationError(f'Status must be one of: {VALID_ROLES}')
-    
-  #   data['role'] = role[0]
-    
   class Meta:
     fields = ('user', 'role')
 
@@ -25,6 +17,14 @@ class AddRoleSchema(ma.Schema):
   user = fields.Nested('UserSchema', only=['id'])
   property = fields.Nested('PropertySchema')
   role = fields.String()
+
+  @validates_schema()
+  def validate_role(self, data, **kwargs):
+    role = [x for x in VALID_ROLES if x.upper() == data['role'].upper()]
+    if len(role) == 0:
+       raise ValidationError(f'Status must be one of: {VALID_ROLES}')
     
+    data['role'] = role[0]    
+
   class Meta:
     fields = ('user_id', 'role')
