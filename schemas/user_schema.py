99c3f1a0dc1185043e8be2dc4638ecd6851a6c61
arharuty <13937@coderacademy.edu.au>
2023-07-03 22:25:18 +1000

    validation on user schema fields

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -4,6 +4,12 @@ from marshmallow import fields
 
 class UserSchema(ma.Schema):
     password = fields.String(validate=Regexp('^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$', error='Password must be at least 8 characters, and must include at least one letter, one number, and one special character.'))
+    first_name = fields.String(validate=Length(min=2))
+    last_name = fields.String(validate=Length(min=2))
+    email = fields.Email()
+    is_admin = fields.Bool(default=False)
+    access = fields.Bool(default=False)
+    
     class Meta:
         fields = ('first_name', 'last_name', 'email', 'password', 'is_admin', 'access', 'id')
         
