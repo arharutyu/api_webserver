30f3d2e5aa83f893d873b253902610f842a00ef3
arharuty <13937@coderacademy.edu.au>
2023-07-04 06:17:32 +1000

    comment model/schema validation

--- schemas/comment_schema.py
+++ schemas/comment_schema.py
@@ -4,9 +4,11 @@ from marshmallow import fields
 class CommentSchema(ma.Schema):
     user = fields.Nested('UserSchema', only=['first_name', 'last_name'])
     item = fields.Nested('ItemSchema', only=['item_name'])
+    comment = fields.String(required=True)
+    date_created = fields.Date()
 
     class Meta:
-        fields = ('comment', 'date_created', 'user', 'item')
+        fields = ('comment', 'date_created', 'user', 'item', 'id')
 
 
 comment_schema = CommentSchema()
