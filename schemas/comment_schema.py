70961a6a25f3789a2a6d2f335c689ec02909d75a
arharuty <13937@coderacademy.edu.au>
2023-07-04 08:49:57 +1000

    comment field max length validation

--- schemas/comment_schema.py
+++ schemas/comment_schema.py
@@ -1,10 +1,11 @@
 from init import ma 
 from marshmallow import fields
+from marshmallow.validate import Length
 
 class CommentSchema(ma.Schema):
     user = fields.Nested('UserSchema', only=['first_name', 'last_name'])
     item = fields.Nested('ItemSchema', only=['item_name'])
-    comment = fields.String(required=True)
+    comment = fields.String(required=True, validate=Length(max=300))
     date_created = fields.Date()
 
     class Meta:
